452072ca1f2bdb32f25ad03c445ffd1df8a756fb
primal-drew <oginaandrew05@gmail.com>
2024-03-19 14:25:36 +0300

    System calls or busts: Showing IP addresses of websites.

---
+++ system_calls_or_bust/showip.cpp
@@ -0,0 +1,60 @@
+#include <string.h>
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <unistd.h>  
+#include <sys/types.h>  
+#include <sys/socket.h>  
+#include <netdb.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[])
+{   
+    int status;
+    struct addrinfo hints;
+    struct addrinfo *p;
+    struct addrinfo *res;
+    char ipstr[INET6_ADDRSTRLEN];
+
+    if(argc !=2)
+    {
+        fprintf(stderr,"Usage:showIP Hostname\n");
+        return 1;
+    }
+
+    memset(&hints,0,sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if((status=getaddrinfo(argv[1],NULL,&hints,&res))!=0)
+    {
+        fprintf(stderr,"getaddrinfo: %s\n",gai_strerror(status));
+        return 2;
+    }
+
+    printf("IP addresses for %s\n\n",argv[1]);
+
+    for(p =res;p !=NULL;p= p->ai_next)
+    {
+        void *addr;
+        char *ipver;
+        
+        if(p->ai_family == AF_INET)
+        {
+            struct sockaddr_in *ipv4 = (sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "ipv4";
+        }
+        else if(p->ai_family == AF_INET6)
+        {
+            struct sockaddr_in6 *ipv6 = (sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "ipv6";
+        }
+
+        inet_ntop(p->ai_family,addr,ipstr,sizeof(ipstr));
+        printf("IP Address: %s\n",ipstr);
+    }
+
+    freeaddrinfo(res);
+    return 0;
+}
